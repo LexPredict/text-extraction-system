f62cbe642f47ac248e282c85ccaf1a3c55b70616
Mikhail Viktorov <mikhail@lexpredict.com>
2020-12-25 21:36:14 +0300

    CS-6086, CS-6087: impl api client and DTO as a separate package

--- install_ubuntu_requirements.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install virtualenv libpq-dev python3-dev img2pdf libreoffice maven
-
-# img2pdf is under LGPLv3. Allows using it as a separate component in the system.
-# img2pdf is executed as a subprocess.
