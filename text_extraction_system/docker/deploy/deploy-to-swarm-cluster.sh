c6bf74d98176f045cb1a76f7b7aa9bde26407423
Mikhail Viktorov <mikhail@lexpredict.com>
2020-12-30 18:56:51 +0300

    CS-6082: make docker image and swarm configs working

--- text_extraction_system/docker/deploy/deploy-to-swarm-cluster.sh
+++ text_extraction_system/docker/deploy/deploy-to-swarm-cluster.sh
@@ -8,6 +8,7 @@ source setenv.sh
 mkdir -p ./temp
 echo "Starting with docker-compose config: ${DOCKER_COMPOSE_FILE}"
 envsubst < ./docker-compose-templates/${DOCKER_COMPOSE_FILE} > ./temp/${DOCKER_COMPOSE_FILE}
+
 docker stack deploy --compose-file ./temp/${DOCKER_COMPOSE_FILE} text_extraction --with-registry-auth
 
 rm -rf ./temp
