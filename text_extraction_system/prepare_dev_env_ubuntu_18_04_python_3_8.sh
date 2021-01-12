9519213087fc22d407a40249a7491c8d3359792f
Mikhail Viktorov <mikhail@lexpredict.com>
2021-01-12 15:22:23 +0300

    CS-6082: add dev env creation scripts/hints for various envs

--- text_extraction_system/prepare_dev_env_ubuntu_18_04_python_3_8.sh
+++ text_extraction_system/prepare_dev_env_ubuntu_18_04_python_3_8.sh
@@ -2,17 +2,18 @@
 
 sudo apt-get install virtualenv libpq-dev python3-dev img2pdf libreoffice maven libpoppler-dev poppler-utils
 
+
 # ensure this is the expected Python executable
-virtualenv -p /usr/local/bin/python3.6 venv
+virtualenv -p /usr/bin/python3 venv
 
 
 source venv/bin/activate
-pip install -U setuptools
 pip install -r requirements.txt
 
 # Assume lexnlp core project is at ../../lexpredict-contraxsuite-core/
 pip install -c requirements.txt -e ../../lexpredict-contraxsuite-core/
 pip install -e ../text_extraction_system_api
+
 
 mkdir models
 wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -O ./models/lid.176.bin
--- text_extraction_system/prepare_dev_env_ubuntu_20_04_python_3_8.sh
+++ text_extraction_system/prepare_dev_env_ubuntu_20_04_python_3_8.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 sudo apt-get install virtualenv libpq-dev python3-dev img2pdf libreoffice maven libpoppler-dev poppler-utils
+
+
+# ensure this is the expected Python executable
 virtualenv -p /usr/bin/python3 venv
+
+
 source venv/bin/activate
 pip install -r requirements.txt
 
 # Assume lexnlp core project is at ../../lexpredict-contraxsuite-core/
-pip install -c requirements.txt -e ../../lexpredict-contraxsuite-core/
+# For some reason -c did not work on 18.04. Installing lexnlp with
+# --no-deps because we already have them in requirements.
+# pip install -c requirements.txt -e ../../lexpredict-contraxsuite-core/
+pip install --no-deps -e ../../lexpredict-contraxsuite-core/
+
 pip install -e ../text_extraction_system_api
 
+
+mkdir models
 wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin -O ./models/lid.176.bin
 
 ./prepare_tika.sh
