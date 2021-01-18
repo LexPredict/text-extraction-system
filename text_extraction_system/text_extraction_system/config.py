25f7114b10d90d38d9c6316f50232d9514dc65e6
Mikhail Viktorov <mikhail@lexpredict.com>
2021-01-18 19:43:09 +0300

    CS-6465: fix java classpath

--- text_extraction_system/text_extraction_system/config.py
+++ text_extraction_system/text_extraction_system/config.py
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     webdav_url: str
     webdav_username: str
     webdav_password: str
-    java_modules_path: str = os.path.join(project_root, './java_modules')
-    fasttext_lang_model: str = os.path.join(project_root, './models/lid.176.bin')
+    java_modules_path: str = os.path.join(project_root, 'java_modules')
+    fasttext_lang_model: str = os.path.join(project_root, 'models/lid.176.bin')
     delete_temp_files_on_request_finish: bool = True
     keep_failed_files: bool = False
 
