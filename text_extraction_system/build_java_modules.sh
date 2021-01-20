19488f311dcd21b11ddc7545febc4f15d92c05c8
Mikhail Viktorov <mikhail@lexpredict.com>
2021-01-20 14:53:31 +0300

    CS-6465: convert to pdf: improve error handling, fix jbig2 dependency

--- text_extraction_system/build_java_modules.sh
+++ text_extraction_system/build_java_modules.sh
@@ -3,6 +3,9 @@
 mkdir -p java_modules
 
 pushd text_extraction_system_java
-mvn clean package
-cp target/text_extraction_system_java-1.0-jar-with-dependencies.jar ../java_modules/
+mvn clean package dependency:copy-dependencies 
+
+rm ../java_modules/*.jar
+cp target/text_extraction_system_java-1.0.jar ../java_modules/
+cp target/dependency/* ../java_modules/
 popd
