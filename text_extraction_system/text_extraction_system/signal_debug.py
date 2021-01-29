d5ac794cc6b35f010f3c0a587a3a17437e577d2b
Mikhail Viktorov <mikhail@lexpredict.com>
2021-01-29 16:24:12 +0300

    CS-6505: temp added service to print signals received

---
+++ text_extraction_system/text_extraction_system/signal_debug.py
@@ -0,0 +1,31 @@
+import datetime
+import os
+import signal
+import threading
+import time
+
+SIGNAL_NAMES_BY_NUM = {int(getattr(signal, sig_name)): sig_name
+                       for sig_name in dir(signal)
+                       if sig_name.startswith('SIG')
+                       and not sig_name.startswith('SIG_')}
+
+
+def receive_signal(sig_num: int, frame):
+    print(f'{datetime.datetime.now().isoformat()} |  Signal: {SIGNAL_NAMES_BY_NUM[sig_num]} ({sig_num})')
+
+
+if __name__ == '__main__':
+    for sig_num in sorted(SIGNAL_NAMES_BY_NUM.keys()):
+        if sig_num == signal.SIGKILL:
+            continue
+        try:
+            signal.signal(sig_num, receive_signal)
+            print(f'Added handler for {SIGNAL_NAMES_BY_NUM[sig_num]} ({sig_num})')
+        except:
+            print(f'Can not add handler for {SIGNAL_NAMES_BY_NUM[sig_num]} ({sig_num})')
+
+    print(f'PID of this process: {os.getpid()}')
+    print(f'Id of this thread: {threading.current_thread().ident}')
+
+    while True:
+        time.sleep(300)
