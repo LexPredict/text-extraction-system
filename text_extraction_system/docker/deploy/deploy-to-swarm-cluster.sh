a48326c5f23e1d3cba404983bf7f9e921c62a90b
Mikhail Viktorov <mikhail@lexpredict.com>
2021-02-04 11:31:11 +0300

    CS-6521: add stack rm to redeploy script

--- text_extraction_system/docker/deploy/deploy-to-swarm-cluster.sh
+++ text_extraction_system/docker/deploy/deploy-to-swarm-cluster.sh
@@ -9,6 +9,7 @@ mkdir -p ./temp
 echo "Starting with docker-compose config: ${DOCKER_COMPOSE_FILE}"
 envsubst < ./docker-compose-templates/${DOCKER_COMPOSE_FILE} > ./temp/${DOCKER_COMPOSE_FILE}
 
+docker stack rm ${TEXT_EXTRACTION_STACK_NAME}
 docker stack deploy --compose-file ./temp/${DOCKER_COMPOSE_FILE} ${TEXT_EXTRACTION_STACK_NAME} --with-registry-auth
 
 rm -rf ./temp
