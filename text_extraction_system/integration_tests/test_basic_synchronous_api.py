cd975e07932eb0766d53f0fb41cd46e84d2fb4a1
DevIhor <ihor.l@coaxsoft.com>
2021-02-24 17:03:10 +0200

    CS-6089: add integration tests to synchronous API extract_all_data_as_zip endpoint and change time delay in synchronous api endpoints

--- text_extraction_system/integration_tests/test_basic_synchronous_api.py
+++ text_extraction_system/integration_tests/test_basic_synchronous_api.py
@@ -1,6 +1,8 @@
 import logging
 import os
+from contextlib import closing
 from unittest import mock
+from zipfile import ZipFile
 
 import pikepdf
 import pytest
@@ -27,7 +29,7 @@ def test_basic_api_extract_plain_text_from_document_expired():
     client = TextExtractionSystemWebClient(test_settings.api_url)
     exception = HTTPError(mock.Mock(status=504), "Input file is too big")
     with pytest.raises(HTTPError) as error_info:
-        with client.extract_plain_text_from_document(fn, convert_to_pdf_timeout_sec=2):
+        with client.extract_plain_text_from_document(fn, full_extract_timeout_sec=2):
             assert error_info == exception
 
 
@@ -46,13 +48,23 @@ def test_extract_text_from_document_and_generate_searchable_pdf_as_local_file_ex
     exception = HTTPError(mock.Mock(status=504), "Input file is too big")
     with pytest.raises(HTTPError) as error_info:
         with client.extract_text_from_document_and_generate_searchable_pdf_as_local_file(
-                fn, convert_to_pdf_timeout_sec=2):
+                fn, full_extract_timeout_sec=2):
             assert error_info == exception
 
 
-# def test_extract_all_data_from_document():
-#     fn = os.path.join(os.path.dirname(__file__), 'data', 'many_pages.odt')
-#     client = TextExtractionSystemWebClient(test_settings.api_url)
-#     resp = client.extract_plain_text_from_document(fn)
-#     for i in range(1, 22):
-#         assert f'This is page {i}' in resp
+def test_extract_all_data_from_document():
+    fn = os.path.join(os.path.dirname(__file__), 'data', 'many_pages.odt')
+    client = TextExtractionSystemWebClient(test_settings.api_url)
+    resp = client.extract_all_data_from_document(fn)
+    with resp as tfn:
+        with closing(ZipFile(tfn)) as archive:
+            assert len(archive.infolist()) == 3
+
+
+def test_extract_all_data_from_document_expired():
+    fn = os.path.join(os.path.dirname(__file__), 'data', 'many_pages.odt')
+    client = TextExtractionSystemWebClient(test_settings.api_url)
+    exception = HTTPError(mock.Mock(status=504), "Input file is too big")
+    with pytest.raises(HTTPError) as error_info:
+        with client.extract_all_data_from_document(fn, full_extract_timeout_sec=2):
+            assert error_info == exception
