87db25777c0925688a73a9e5e5b5f3044cb52e30
Andrey <andrey.sitaev@gmail.com>
2021-03-02 15:42:42 +0300

    CS-6461: update build scripts

--- text_extraction_system/build_spa.dev.sh
+++ text_extraction_system/build_spa.dev.sh
@@ -2,12 +2,9 @@
 
 cd ../text_extraction_system_ui
 echo "API_HOST=127.0.0.1:8000
-API_PROTOCOL=https" >> .env
+API_PROTOCOL=http" >> .env
 npm run build
 
 cp build/index.html ../text_extraction_system/text_extraction_system/templates
 cd ../text_extraction_system/text_extraction_system/templates
 sed -i 's/src=".\/bundle.js"/src="static\/bundle.js"/g' index.html
-
-
-
