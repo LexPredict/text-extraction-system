2a6368cc6ec8e9109695c7cb5018454399883f0a
Andrey <andrey.sitaev@gmail.com>
2021-03-02 13:25:54 +0300

    CS-6461: improve request rendering

--- text_extraction_system_ui/src/store/tasks.ts
+++ text_extraction_system_ui/src/store/tasks.ts
@@ -65,7 +65,6 @@ export default class {
                 newTasks.push(ut);
             });
             newTasks = this.sortAndFilterRequests(newTasks);
-            console.log(newTasks);
             runInAction(() => {
                 this.tasks.replace(newTasks);
             });
