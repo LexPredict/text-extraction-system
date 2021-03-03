690b0e98d8374257304abd0e9011e9c860b238e7
Andrey <andrey.sitaev@gmail.com>
2021-03-03 16:24:05 +0300

    CS-6461: modify docker image for React subdirectory pattern for future replacement

--- text_extraction_system/docker/build/start.template.sh
+++ text_extraction_system/docker/build/start.template.sh
@@ -74,6 +74,10 @@ elif [ "${DOLLAR}{ROLE}" == "signal-debug" ]; then
   exec python3 text_extraction_system/signal_debug.py
 elif [ "${DOLLAR}{ROLE}" == "web-api" ]; then
   startup
+  pushd .
+  cd text_extraction_system/templates
+  sed -i "s/TEXT_EXTRACTION_SPA_SUBDIRECTORY/${DOLLAR}{TEXT_EXTRACTION_SYSTEM_ROOT_PATH}/g" bundle.js
+  popd
   exec uvicorn --host 0.0.0.0 --port 8000 --root-path ${DOLLAR}{text_extraction_system_root_path} text_extraction_system.web_api:app
 elif [ "${DOLLAR}{ROLE}" == "celery-worker" ]; then
   startup
