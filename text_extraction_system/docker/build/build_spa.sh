3469131ecb9bbe91d2eb36c664850d9258fe7cfd
Andrey <andrey.sitaev@gmail.com>
2021-03-03 12:15:41 +0300

    CS-6461: add build debug info

--- text_extraction_system/docker/build/build_spa.sh
+++ text_extraction_system/docker/build/build_spa.sh
@@ -4,6 +4,10 @@ cd ../../text_extraction_system_ui
 npm install
 npm run build
 
+echo "Current folder:"
+ls -l
+pwd
+
 cp build/index.html ../text_extraction_system/templates
 cd ../text_extraction_system/templates
 sed -i 's/src=".\/bundle.js"/src="static\/bundle.js"/g' index.html
