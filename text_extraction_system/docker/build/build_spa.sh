b5165d58bcd7a1b408fd04e9479325dcbb595e0c
Andrey <andrey.sitaev@gmail.com>
2021-03-03 12:22:49 +0300

    CS-6461: add build debug info

--- text_extraction_system/docker/build/build_spa.sh
+++ text_extraction_system/docker/build/build_spa.sh
@@ -5,8 +5,17 @@ npm install
 npm run build
 
 echo "Current folder:"
-ls -l
 pwd
+ls -l
+
+echo "Parent folder:"
+ls -l ../
+
+echo "Build folder:"
+ls -l build
+
+echo "Parent folder/TE:"
+ls -l ../text_extraction_system
 
 cp build/index.html ../text_extraction_system/templates
 cd ../text_extraction_system/templates
