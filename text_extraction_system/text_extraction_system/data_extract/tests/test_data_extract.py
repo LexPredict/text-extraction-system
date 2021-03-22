c93d398c99cce2990d77bd4af03b5fa05d4b68fa
Mikhail Viktorov <mikhail@lexpredict.com>
2021-03-22 11:58:03 +0300

    CS-6591: fix table detection in OCRed PDFs

--- text_extraction_system/text_extraction_system/data_extract/tests/test_data_extract.py
+++ text_extraction_system/text_extraction_system/data_extract/tests/test_data_extract.py
@@ -1,7 +1,11 @@
 import os
+import warnings
+from unittest.mock import MagicMock
 
 from text_extraction_system.commons.tests.commons import with_default_settings
 from text_extraction_system.data_extract import data_extract
+from text_extraction_system.data_extract.tables import extract_tables
+from text_extraction_system.pdf.pdf import merge_pdf_pages, extract_page_images
 
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
@@ -90,3 +94,36 @@ def test_different_languages_extraction_with_no_ocr():
     assert len(struct.sentences) == 2
     for i in struct.sentences:
         assert i.language == struct.language
+
+
+@with_default_settings
+def test_table_ocr():
+    fn = os.path.join(data_dir, 'table1.png')
+    warn_mock = MagicMock('warn')
+    warnings.warn = warn_mock
+
+    from text_extraction_system.ocr.ocr import ocr_page_to_pdf
+
+    with ocr_page_to_pdf(fn) as pdf_fn:
+        with open(pdf_fn, 'rb') as ocred_in_file:
+            ocred_page_layout = data_extract.get_first_page_layout(ocred_in_file)
+            camelot_tables = extract_tables(1, ocred_page_layout, fn)
+
+    assert len(camelot_tables) == 1
+    warn_mock.assert_not_called()
+
+
+@with_default_settings
+def test_table_warnings():
+    fn = os.path.join(data_dir, 'camelot_warn.pdf')
+    warn_mock = MagicMock('warn')
+    warnings.warn = warn_mock
+
+    with extract_page_images(fn, 1, 1) as image_fns:
+        image_fn = image_fns[0]
+        with open(fn, 'rb') as ocred_in_file:
+            ocred_page_layout = data_extract.get_first_page_layout(ocred_in_file)
+            camelot_tables = extract_tables(1, ocred_page_layout, image_fn)
+
+    assert len(camelot_tables) == 1
+    warn_mock.assert_not_called()
