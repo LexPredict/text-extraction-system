f48c1e293e0ed539df4ab7202b39c51c51ea510b
Mikhail Viktorov <mikhail@lexpredict.com>
2021-03-22 13:58:03 +0300

    CS-6694: add unit test for blurry doc

--- text_extraction_system/text_extraction_system/ocr/tests/test_ocr.py
+++ text_extraction_system/text_extraction_system/ocr/tests/test_ocr.py
@@ -3,7 +3,7 @@ import os
 from text_extraction_system.commons.tests.commons import with_default_settings
 from text_extraction_system.data_extract.data_extract import extract_text_pdfminer
 from text_extraction_system.ocr.ocr import ocr_page_to_pdf
-from text_extraction_system.pdf.pdf import extract_page_images
+from text_extraction_system.pdf.pdf import extract_page_images, extract_page_ocr_images
 
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
 
@@ -21,3 +21,14 @@ def test_ocr_page():
     assert 'You may add Your own' in txt
     assert 'Submission of Contributions' in txt
     assert 'END OF TERMS AND CONDITIONS' in txt
+
+
+@with_default_settings
+def test_ocr_blurry():
+    fn = os.path.join(data_dir, 'blurred_noisy_scan.pdf')
+    txt = ''
+    with extract_page_ocr_images(fn, 1, 1) as image_fns:
+        image_with_text, image_no_text = image_fns[0]
+        with ocr_page_to_pdf(image_with_text) as pdf_fn:
+            txt += '\n' + extract_text_pdfminer(pdf_fn)
+    assert 'Approved  For  Release' in txt
