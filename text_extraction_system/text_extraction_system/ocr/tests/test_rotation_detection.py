c283fb097fb6cb1a17e1991e5ddecb816305448a
Mikhail Viktorov <mikhail@lexpredict.com>
2021-04-13 15:57:10 +0300

    CS-6817: switch deskew to dilated rows method as we already able to re-orient the page before it

--- text_extraction_system/text_extraction_system/ocr/tests/test_rotation_detection.py
+++ text_extraction_system/text_extraction_system/ocr/tests/test_rotation_detection.py
@@ -1,8 +1,7 @@
 import os
-from math import floor
 
 from text_extraction_system.commons.tests.commons import with_default_settings
-from text_extraction_system.ocr.rotation_detection import determine_skew
+from text_extraction_system.ocr.rotation_detection import determine_skew, detect_rotation_dilated_rows
 from text_extraction_system.pdf.pdf import extract_page_images
 
 data_dir = os.path.join(os.path.dirname(__file__), 'data')
@@ -12,8 +11,14 @@ data_dir = os.path.join(os.path.dirname(__file__), 'data')
 def test_angle1():
     fn = os.path.join(data_dir, 'deskew_goes_crazy.png')
     actual = determine_skew(fn)
+    assert int(actual) == 0
 
-    assert abs(actual) < 10
+
+@with_default_settings
+def test_angle1_dilated_rows():
+    fn = os.path.join(data_dir, 'deskew_goes_crazy.png')
+    angle = detect_rotation_dilated_rows(fn, pre_calculated_orientation=None)
+    assert int(angle) == 0
 
 
 @with_default_settings
@@ -21,7 +26,15 @@ def test_angle2():
     fn = os.path.join(data_dir, 'rotated1.pdf')
     with extract_page_images(fn, 1, 1) as png_fns:
         angle = determine_skew(png_fns[0])
-        assert 5 < abs(angle) < 7
+        assert int(angle) == -6
+
+
+@with_default_settings
+def test_angle2_dilated_rows():
+    fn = os.path.join(data_dir, 'rotated1.pdf')
+    with extract_page_images(fn, 1, 1) as png_fns:
+        angle = detect_rotation_dilated_rows(png_fns[0], pre_calculated_orientation=None)
+        assert int(angle) == -5
 
 
 @with_default_settings
@@ -29,11 +42,26 @@ def test_angle3():
     fn = os.path.join(data_dir, 'rotated_small_angle.pdf')
     with extract_page_images(fn, 1, 1) as png_fns:
         angle = determine_skew(png_fns[0])
-        assert floor(abs(angle)) == 2
+        assert int(angle) == -2
+
+
+@with_default_settings
+def test_angle3_dilated_rows():
+    fn = os.path.join(data_dir, 'rotated_small_angle.pdf')
+    with extract_page_images(fn, 1, 1) as png_fns:
+        angle = detect_rotation_dilated_rows(png_fns[0], pre_calculated_orientation=None)
+        assert int(angle) == -2
 
 
 @with_default_settings
 def test_angle4():
     fn = os.path.join(data_dir, 'realdoc__00121.png')
     angle = determine_skew(fn)
-    assert floor(abs(angle)) == 88
+    assert int(angle) == -88
+
+
+@with_default_settings
+def test_angle4_dilated_rows():
+    fn = os.path.join(data_dir, 'realdoc__00121.png')
+    angle = detect_rotation_dilated_rows(fn, pre_calculated_orientation=None)
+    assert int(angle) == -88
