eaa92e3eaf0bcbae3fa5e65fb87f74aac5a9fe28
Mikhail Viktorov <mikhail@lexpredict.com>
2021-04-13 15:57:10 +0300

    CS-6817: switch deskew to dilated rows method as we already able to re-orient the page before it

--- text_extraction_system/text_extraction_system/data_extract/data_extract.py
+++ text_extraction_system/text_extraction_system/data_extract/data_extract.py
@@ -27,8 +27,8 @@ from pdfminer.pdfparser import PDFParser
 from text_extraction_system.config import get_settings
 from text_extraction_system.data_extract.camelot.camelot import extract_tables
 from text_extraction_system.data_extract.lang import get_lang_detector
-from text_extraction_system.ocr.ocr import ocr_page_to_pdf, rotate_image, orientation_and_script_detected
-from text_extraction_system.ocr.rotation_detection import determine_skew
+from text_extraction_system.ocr.ocr import ocr_page_to_pdf, rotate_image, image_to_osd, OSD
+from text_extraction_system.ocr.rotation_detection import detect_rotation_dilated_rows
 from text_extraction_system.pdf.pdf import page_requires_ocr, extract_page_ocr_images, \
     raise_from_pdfbox_error_messages, merge_pdf_pages
 from text_extraction_system.processes import raise_from_process
@@ -210,9 +210,12 @@ def process_pdf_page(pdf_fn: str,
             original_page_layout = get_first_page_layout(in_file)
 
             if ocr_enabled and page_requires_ocr(original_page_layout):
-                if orientation_and_script_detected(page_image_with_text_fn):
+                osd: OSD = image_to_osd(page_image_with_text_fn)
+                if osd.script and osd.script_conf > 1 and osd.orientation_conf > 1:
                     # this detects scanned text rotation angle
-                    angle: Optional[float] = determine_skew(page_image_with_text_fn) if deskew_enabled else None
+                    angle: Optional[float] = detect_rotation_dilated_rows(page_image_with_text_fn,
+                                                                          pre_calculated_orientation=osd.orientation) \
+                        if deskew_enabled else None
                     # rotate_image() will pass as is if the angle is None
                     with rotate_image(page_image_with_text_fn, angle, DPI,
                                       align_to_closest_90=True) as page_image_with_text_fn, \
