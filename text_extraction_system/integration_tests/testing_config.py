5e22b8d2b82f9066233af26d8355a69ae1994acd
Andrew Parsons <parsonsandrew1@gmail.com>
2021-06-25 14:25:09 -0400

    CS-7171: update integration tests

--- text_extraction_system/integration_tests/testing_config.py
+++ text_extraction_system/integration_tests/testing_config.py
@@ -2,9 +2,11 @@ from pydantic import BaseSettings
 
 
 class TestingSettings(BaseSettings):
-    api_url: str
-    call_back_server_bind_host: str
-    call_back_server_bind_port: int
+    api_url: str = 'http://127.0.0.1:8000'
+    call_back_server_bind_host: str = '127.0.0.1'
+    call_back_server_bind_port: int = 54321
+    username: str = None
+    password: str = None
 
     class Config:
         env_prefix = 'testing_'
