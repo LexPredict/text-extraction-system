8bd14fd757fb0cfaf111fcf9fbadec1ca95f3bd7
Andrew Parsons <parsonsandrew1@gmail.com>
2021-06-25 14:27:19 -0400

    CS-7171: remove accidentally pushed code

--- text_extraction_system/integration_tests/testing_config.py
+++ text_extraction_system/integration_tests/testing_config.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings
 
 
 class TestingSettings(BaseSettings):
-    api_url: str = 'http://127.0.0.1:8000'
-    call_back_server_bind_host: str = '127.0.0.1'
-    call_back_server_bind_port: int = 54321
+    api_url: str
+    call_back_server_bind_host: str
+    call_back_server_bind_port: int
     username: str = None
     password: str = None
 
