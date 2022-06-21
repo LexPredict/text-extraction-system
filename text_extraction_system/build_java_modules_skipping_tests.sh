50d0b37528df47e14ecf93baea6120b08139355d
DevIhor <ihor.l@coaxsoft.com>
2022-06-21 11:45:34 +0300

    AD-172: add script for building java modules without tests

---
+++ text_extraction_system/build_java_modules_skipping_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p java_modules
+
+pushd text_extraction_system_java
+mvn clean package dependency:copy-dependencies -Dmaven.test.skip
+
+rm ../java_modules/*.jar
+cp target/text_extraction_system_java-1.2.jar ../java_modules/
+cp target/dependency/* ../java_modules/
+popd
