d02ba665a2842c7babf6bccc8ea8980cfab18c7f
Andrew Parsons <parsonsandrew1@gmail.com>
2022-08-08 15:10:25 -0400

    CMML-65: update function call for get_paragraphs

--- text_extraction_system/text_extraction_system/data_extract/data_extract.py
+++ text_extraction_system/text_extraction_system/data_extract/data_extract.py
@@ -14,7 +14,7 @@ from tempfile import mkdtemp
 from typing import Tuple, Generator, Optional, Dict, Any, List
 
 import msgpack
-from lexnlp.nlp.en.segments.paragraphs import get_paragraphs
+from lexnlp.nlp.en.segments.paragraphs import get_paragraph_spans
 from lexnlp.nlp.en.segments.sections import get_document_sections_with_titles
 from lexnlp.nlp.en.segments.sentences import get_sentence_span_list
 from lexnlp.nlp.en.segments.titles import get_titles
@@ -160,7 +160,7 @@ def extract_text_and_structure(pdf_fn: str,
         paragraphs = [PlainTextParagraph(start=start,
                                          end=end,
                                          language=language or lang.predict_lang(segment))
-                      for segment, start, end in get_paragraphs(text, return_spans=True)]
+                      for start, end, segment, in get_paragraph_spans(text)]
 
         if read_sections_from_toc and table_of_contents:
             sections = get_sections_from_table_of_contents(table_of_contents,
