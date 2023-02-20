1144a1523f30d8cd2f29eec2270836efd6aea7a1
TUshar Bhavar <tushar.bhavar@elevateservices.com>
2023-02-20 15:12:33 -0500

    MC-464 : cannot-upload-pd-fs-because-of-errors-in-tes

--- text_extraction_system/text_extraction_system/commons/sysutils.py
+++ text_extraction_system/text_extraction_system/commons/sysutils.py
@@ -3,5 +3,9 @@ import sys
 
 
 def increase_recursion_limit():
-    resource.setrlimit(resource.RLIMIT_STACK, (10 * (2 ** 20), 10 * (2 ** 20)))
+    current_val = resource.getrlimit(resource.RLIMIT_STACK)[0]
+    try:
+        resource.setrlimit(resource.RLIMIT_STACK, (current_val, -1))
+    except:
+        resource.setrlimit(resource.RLIMIT_STACK, (current_val, current_val))
     sys.setrecursionlimit(50000)
